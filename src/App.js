74b70b932f7b93ecdd44a0eb62cb3d5a96850e78
poongkabilanvrp <poongkabilan1999@gmail.com>
2021-07-11 20:07:41 +0530

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MaintenancePage from './Components/MaintenancePage'
 
 function App() {
   return (
     <div className="App">
-      <h1>Server under maintenance</h1>
-      <h3>vivegaaschool cbse</h3>
+      <MaintenancePage/>
     </div>
   );
 }
---
+++ src/Components/MaintenancePage.js
@@ -0,0 +1,12 @@
+import './MaintenancePage.css'
+
+function MaintenancePage(){
+  return(
+    <div className='Down-page'>
+      <h1>Server is down for maintenance</h1>
+      <h3>  - Vivegaa School CBSE</h3>
+    </div>
+  )
+}
+
+export default MaintenancePage
